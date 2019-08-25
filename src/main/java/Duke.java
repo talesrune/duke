e68ff6d3b66f46ff881429ada75fed57c910f425
talesrune <talesrune@gmail.com>
2019-08-25 17:58:03 +0800

    Updated Level 3 with class, replacing string and boolean arrays

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList; //to use arraylist
+//import java.util.*;
 
 public class Duke {
     /**Hi.*/ //1. indentation 2. must have full-stop 3. special commenting
@@ -12,41 +14,37 @@ public class Duke {
         System.out.println(line);
         Scanner input = new Scanner(System.in);
         String word;
-        String[] items = new String[100];
-        boolean[] checkitem = new boolean[100];
-        int count = 0;
-        //test tasks
-        //items[0] = "read book";
-        //items[1] = "return book";
-        //items[2] = "buy bread";
-        //count = 3;
-        //test tasks end
+
+        Task t = new Task("read book");
+        Task u = new Task("return book");
+        t.setStatusIcon(true);
+        Task v = new Task("buy bread");
+
+        ArrayList<Task> items = new ArrayList<Task>();
+        items.add(t);
+        items.add(u);
+        items.add(v);
 
         while (true) {
             word = input.nextLine();
             String[] arr = word.split(" ");
             System.out.println(line);
+            Task temp; //temp object
+
             if (word.equals("list")) {
                 System.out.println("     Here are the tasks in your list:");
-                for (int i = 0; i < count; i++) {
-                    String tc;
-                    if (!checkitem[i]) {
-                        tc = cross;
-                    } else {
-                        tc = tick;
-                    }
-                    System.out.println("     " + (i + 1) + "." + tc  + " " + items[i]);
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println("     " + (i + 1) + "." + items.get(i).getStatusIcon()
+                            + " " + items.get(i).getDescription());
                 }
-
             } else if (arr[0].equals("done")) {
-                String tc = tick;
-                checkitem[Integer.parseInt(arr[1]) - 1] = true;
+                int tasknum = Integer.parseInt(arr[1]) - 1;
+                items.get(tasknum).setStatusIcon(true);
                 System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       " + tc + " " + items[Integer.parseInt(arr[1]) - 1]);
-
+                System.out.println("       " + tick + " " + items.get(tasknum).getDescription());
             } else if (!word.equals("bye") && !word.isEmpty()) {
-                items[count] = word;
-                count++;
+                temp = new Task(word);
+                items.add(temp);
                 System.out.println("     added: " + word);
             } else if (word.equals("bye")) {
                 System.out.println("     Bye. Hope to see you again soon!");
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    //public void setDescription(String description) {
+    //}
+
+    public String getStatusIcon() {
+        return (isDone ? "[✓]" : "[✗]"); //return tick or X symbols
+    }
+
+    public void setStatusIcon(boolean setDone) {
+        isDone = setDone;
+    }
+
+}
