66255c5a63e10b47f3ce80c71f8e2439bf4a40ff
talesrune <talesrune@gmail.com>
2019-08-25 12:21:12 +0800

    Completed Level 1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     /**Hi.*/ //1. indentation 2. must have full-stop 3. special commenting
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "    ____________________________________________________________";
+        System.out.println(line);
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println(line);
+        Scanner input = new Scanner(System.in);
+        String word;
+        while (true) {
+            word = input.nextLine();
+            System.out.println(line);
+            if (!word.equals("bye")) {
+                System.out.println("     " + word);
+            } else {
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+        }
     }
 }
