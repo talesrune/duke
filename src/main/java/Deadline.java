3996b6e0457c309f00fd3398171a72f3075b04f2
talesrune <talesrune@gmail.com>
2019-08-28 20:24:30 +0800

    Completed Level 8. Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,58 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
+
 public class Deadline extends Task {
+    protected Date by;
+    protected String[] suf = { "st", "nd", "rd", "th" };
+    protected SimpleDateFormat datetimeFormat = new SimpleDateFormat("dd/MM/yyyy HHmm");
 
-    protected String by;
-
-    public Deadline(String description, String by) {
+    /**Hi.*/
+    public Deadline(String description, String by) throws ParseException {
         super(description);
-        this.by = by;
+        Date dateTime = null;
+        try {
+            dateTime = datetimeFormat.parse(by);
+            this.by = dateTime;
+        } catch (ParseException e) {
+            System.out.println("Error reading date/time, please use this format \"d/MM/yyyy HHmm\"");
+            throw e;
+        }
+
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        SimpleDateFormat datetimeFormat2 = new SimpleDateFormat("MMMMM yyyy, h:mm a");
+        SimpleDateFormat datetimeFormat3 = new SimpleDateFormat("MMMMM yyyy, ha");
+        String displayDT = "";
+
+        int day = Integer.parseInt(new SimpleDateFormat("d").format(by));
+        int min = Integer.parseInt(new SimpleDateFormat("m").format(by));
+        if (min > 0) {
+            displayDT = datetimeFormat2.format(by);
+        } else {
+            displayDT = datetimeFormat3.format(by);
+        }
+        int sufIndex = -1;
+
+        if (day == 1 || day == 21 || day == 31) {
+            sufIndex = 0;
+        } else if (day == 2 || day == 22) {
+            sufIndex = 1;
+        } else if (day == 3 || day == 23) {
+            sufIndex = 2;
+        } else if (day > 3 && day < 31) {
+            sufIndex = 3;
+        }
+        String suffixStr = day + suf[sufIndex];
+        displayDT = suffixStr + " of " + displayDT;
+        return "[D]" + super.toString() + " (by: " + displayDT + ")";
     }
 
     @Override
     public String toFile() {
-        return "D|" + super.toFile() + "|" + by;
+        String datetimeStr = datetimeFormat.format(by);
+        return "D|" + super.toFile() + "|" + datetimeStr;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,60 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
+
 public class Event extends Task {
 
-    protected String at;
+    protected Date at;
+    protected String[] suf = { "st", "nd", "rd", "th" };
+    protected SimpleDateFormat datetimeFormat = new SimpleDateFormat("dd/MM/yyyy HHmm");
 
-    public Event(String description, String at) {
+    /**Hi.*/
+    public Event(String description, String at) throws ParseException {
         super(description);
-        this.at = at;
+
+        Date dateTime = null;
+        try {
+            dateTime = datetimeFormat.parse(at);
+            this.at = dateTime;
+        } catch (ParseException e) {
+            System.out.println("Error reading date/time, please use this format \"d/MM/yyyy HHmm\"");
+            throw e;
+        }
+
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        SimpleDateFormat datetimeFormat2 = new SimpleDateFormat("MMMMM yyyy, h:mm a");
+        SimpleDateFormat datetimeFormat3 = new SimpleDateFormat("MMMMM yyyy, ha");
+        String displayDT = "";
+
+        int day = Integer.parseInt(new SimpleDateFormat("d").format(at));
+        int min = Integer.parseInt(new SimpleDateFormat("m").format(at));
+        if (min > 0) {
+            displayDT = datetimeFormat2.format(at);
+        } else {
+            displayDT = datetimeFormat3.format(at);
+        }
+        int sufIndex = -1;
+
+        if (day == 1 || day == 21 || day == 31) {
+            sufIndex = 0;
+        } else if (day == 2 || day == 22) {
+            sufIndex = 1;
+        } else if (day == 3 || day == 23) {
+            sufIndex = 2;
+        } else if (day > 3 && day < 31) {
+            sufIndex = 3;
+        }
+        String suffixStr = day + suf[sufIndex];
+        displayDT = suffixStr + " of " + displayDT;
+        return "[E]" + super.toString() + " (at: " + displayDT + ")";
     }
 
     @Override
     public String toFile() {
-        return "E|" + super.toFile() + "|" + at;
+        String datetimeStr = datetimeFormat.format(at);
+        return "E|" + super.toFile() + "|" + datetimeStr;
     }
 }
